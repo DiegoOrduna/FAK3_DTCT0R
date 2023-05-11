2bb98fe4ed0be3486723a2aa1677ad61e919bc28
DiegoOrduna <odiegotoni@gmail.com>
2023-05-11 00:46:34 -0600

    showing results in popup

--- popup.js
+++ popup.js
@@ -1,6 +1,12 @@
 // Función para detectar deepfake en la imagen o el video
 
 async function detectDeepfake(file) {
+  try {
+    document.getElementById("div_result").remove();
+  } catch (error) {
+    console.log(error);
+  }
+
   //Se carga el modelo MesoNet con TensorFlow.js
   const model = await tf.loadLayersModel("./model.json");
   // model.summary();
@@ -19,11 +25,65 @@ async function detectDeepfake(file) {
   //se obtiene el valor de la prediccion
   prediction = prediction.dataSync()[0];
   console.log("prediction:");
-  console.log(prediction > 0.5);
+  console.log(prediction > 0.75);
   console.log(prediction);
 
+  //se obtiene analyzedImage y se agrega el resultado de la prediccion como h5 y p al final de la pagina
+
+  let analyzedImage;
+
+  if (document.getElementById("analyzedImage")) {
+    analyzedImage = document.getElementById("analyzedImage");
+    document.getElementById("analyzedImage").remove();
+  }
+
+  let h3 = document.createElement("h5");
+  let p = document.createElement("p");
+  h3.innerHTML = `Resultado del análisis: ${
+    prediction > 0.75 ? "Real" : "DeepFake"
+  }`;
+  h3.style.color = prediction > 0.75 ? "green" : "red";
+  h3.id = "h3_result";
+  p.id = "p_result";
+  p.innerHTML = prediction;
+
+  let div = document.createElement("div");
+  div.id = "div_result";
+  div.style.display = "flex";
+  div.style.flexDirection = "column";
+  div.style.alignItems = "center";
+  div.style.justifyContent = "center";
+
+  div.appendChild(document.createElement("br"));
+  div.appendChild(analyzedImage);
+  div.appendChild(h3);
+  div.appendChild(p);
+  div.appendChild(document.createElement("br"));
+
+  document.body.appendChild(div);
+
+  //se muestra una notificacion con el resultado del analisis
+
+  notifyResult(prediction > 0.75);
+
   //se retorna si la imagen es DeepFake o no
-  return prediction > 0.5;
+  return prediction > 0.75;
+}
+
+//Funcion para generar una chrome.notification con el resultado del analisis. En caso de que el resultado sea positivo, se muestra una notificacion de deepfake, en caso contrario, se muestra una notificacion de que no es deepfake. Si no se ha subido un archivo o se ha ingresado una URL, se muestra una notificacion de error.
+function notifyResult(isReal) {
+  let message;
+  if (isReal) {
+    message = "El archivo analizado no es un deepfake";
+  } else {
+    message = "El archivo analizado es un deepfake";
+  }
+  chrome.notifications.create({
+    type: "basic",
+    title: "Resultado del análisis",
+    message: message,
+    iconUrl: "icon.png",
+  });
 }
 
 async function getTensor(file) {
@@ -56,12 +116,14 @@ async function getTensor(file) {
 
   //se crea un objeto de tipo canvas para reescalar la imagen
   let canvas = document.createElement("canvas");
+  canvas.id = "analyzedImage";
   let ctx = canvas.getContext("2d");
 
   //se reescala la imagen
   canvas.width = 256;
   canvas.height = 256;
   ctx.drawImage(img, 0, 0, 256, 256);
+
   document.body.appendChild(canvas);
 
   //se obtiene el tensor de la imagen
@@ -119,66 +181,33 @@ analyzeBtn.addEventListener("click", function () {
       if (file.type.startsWith("image/") || file.type.startsWith("video/")) {
         // Analizar el archivo en busca de deepfake
         isReal = detectDeepfake(file);
-        if (isReal) {
-          message = "El archivo analizado no es un deepfake";
-        } else {
-          message = "El archivo analizado es un deepfake";
-        }
-      } else {
-        message = "Por favor, cargue un archivo de imagen o video";
+        console.log(isReal);
       }
-      // Mostrar una notificación con el resultado del análisis
-      chrome.notifications.create({
-        type: "basic",
-        title: "Resultado del análisis // URL",
-        message: message,
-        iconUrl: "icon.png",
-      });
     });
   } else {
     if (file.type.startsWith("image/") || file.type.startsWith("video/")) {
       // Analizar el archivo en busca de deepfake
       isReal = detectDeepfake(file);
       console.log(isReal);
-      if (isReal) {
-        message = "El archivo analizado no es un deepfake";
-      } else {
-        message = "El archivo analizado es un deepfake";
-      }
-    } else {
-      message = "Por favor, cargue un archivo de imagen o video";
     }
-    chrome.notifications.create({
-      type: "basic",
-      title: "Resultado del análisis // File",
-      message: message,
-      iconUrl: "icon.png",
-    });
   }
 });
 
-// Crear un menú contextual para la extensión
-
+// Menu contextual para analizar imágenes y videos. En esta seccion se crea el menu contextual y se le asigna la funcion de analizar el archivo seleccionado.
 chrome.contextMenus.create({
-  title: "Analizar imagen o video",
+  title: "Analizar DeepFake",
   contexts: ["image", "video"],
-  onclick: function (info) {
-    // console.log("info", info);
-    var url = info.srcUrl;
-    var file = { name: url.split("/").pop(), type: "" };
-    // Analizar la imagen o el video en busca de deepfake
-    isReal = detectDeepfake(file) > 0.5;
-    if (isReal) {
-      message = "La imagen o el video analizado es un deepfake";
-    } else {
-      message = "La imagen o el video analizado no es un deepfake";
-    }
-    // Mostrar una notificación con el resultado del análisis
-    chrome.notifications.create({
-      type: "basic",
-      title: "Resultado del análisis",
-      message: message,
-      iconUrl: "icon.png",
+  onclick: function (info, tab) {
+    // Se obtiene el archivo seleccionado
+    let url = info.srcUrl;
+    let fileName = url.split("/").pop();
+    let file = getFileFromURL(url, fileName).then((file) => {
+      // Verificar que el archivo es una imagen o un video
+      if (file.type.startsWith("image/") || file.type.startsWith("video/")) {
+        // Analizar el archivo en busca de deepfake
+        isReal = detectDeepfake(file);
+        console.log(isReal);
+      }
     });
   },
 });
