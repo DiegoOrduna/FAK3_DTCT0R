a620bf1e43e9d78249d68c130d7de74f68481fb2
DiegoOrduna <odiegotoni@gmail.com>
2023-05-11 21:28:32 -0600

    removing unused files

--- options.js
+++
@@ -1,6 +0,0 @@
-// const nameInput = document.getElementById("name-input");
-// const saveBtn = document.getElementById("save-btn");
-
-// saveBtn.addEventListener("click", () => {
-//   console.log(nameInput.value);
-// });
